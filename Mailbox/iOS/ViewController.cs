b6e034eeec60063df139d06a9bf10b9d94c68f45
Fadoua Ait Amrou <faitamrou.90@gmail.com>
2020-06-27 20:40:16 -0400

    Added a tableview

---
+++ Mailbox/iOS/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace Mailbox.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        int count = 1;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        UITableView tableView;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            tableView = new UITableView(this.View.Frame);
+            this.View.Add(tableView);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.		
+        }
+    }
+}
